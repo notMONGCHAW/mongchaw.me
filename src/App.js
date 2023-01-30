3084e406d18a25376d29abff14d0e3fbc85a1d03
MONGCHAW <provabben@gmail.com>
2023-01-31 01:38:11 +0600

    fixed app.js due to miss merge mangement

--- src/App.js
+++ src/App.js
@@ -6,25 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div class="flex">
-        <img src={logo} class="flex-none border" className="App-logo border" alt="logo" />
-        <img src={tailwindLogo} class="flex-1 animate-bounce border" alt="logo" />
-      </div>
-        <h2>
-          Hi 
-        </h2>
-        <br />
-        <h1>
-          I am MONGCHAW 
-        </h1>
-        <br />
-        <p class="animate-pulse">
-          This site uses react.js and tailwindcss
-        </p>
-        <p>
-          I will update this website some day in the future !!
-        </p>
-        <br />
+        <div class="flex">
+          <img
+            src={logo}
+            class="flex-none border"
+            className="App-logo "
+            alt="logo"
+          />
+          <img src={tailwindLogo} class="misc-logo flex-1 animate-bounce " alt="logo" />
+        </div>
         <div>
           <h1 class="text-5xl">Hi</h1>
           <h1 class="text-3xl space-x-4">I am MONGCHAW</h1>
@@ -42,27 +32,27 @@ function App() {
               </a>
             </button>
           </div>
-          <br/>
-          <div>
-          <button
-          class="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded space-x-4"
-          type="button"
-        >
-          <a href="https://www.youtube.com/@notmongchaw4619/">
-            My youtube channel
-          </a>
-        </button>
-        </div>
-          <br/>
+          <br />
           <div>
             <button
-            class="bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded space-x-4"
-            type="button"
-          >
-            <a href="https://therickroll.com/">
-              A cool website with no HTML (firefox only)
-            </a>
-          </button>
+              class="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded space-x-4"
+              type="button"
+            >
+              <a href="https://www.youtube.com/@notmongchaw4619/">
+                My youtube channel
+              </a>
+            </button>
+          </div>
+          <br />
+          <div>
+            <button
+              class="bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded space-x-4"
+              type="button"
+            >
+              <a href="https://therickroll.com/">
+                A cool website with no HTML (firefox only)
+              </a>
+            </button>
           </div>
         </div>
       </header>
